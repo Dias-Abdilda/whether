ec80635e354a5b246b0f08cf4f0618f563ac77a2
Dias-Abdilda <92967061+Dias-Abdilda@users.noreply.github.com>
2022-11-14 14:55:49 +0600

    create a new fragment

---
+++ app/src/main/java/com/example/whether2/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.whether2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.whether2.R
+import fragments.mainFragment
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    supportFragmentManager
+        .beginTransaction()
+        .replace(R.id.placeHolder, mainFragment.newInstance())
+        .commit()
+    }
+}
