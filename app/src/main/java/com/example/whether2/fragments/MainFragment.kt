f575a02ce2a0b68283a6cf128e4583f786608f75
Assanali_Rakhmetulla <200107012@stu.sdu.edu.kz>
2022-11-21 13:59:37 +0600

    My commiit

--- app/src/main/java/com/example/whether2/fragments/MainFragment.kt
+++ app/src/main/java/com/example/whether2/fragments/MainFragment.kt
@@ -1,14 +1,19 @@
 package com.example.whether2.fragments
 
+import android.Manifest
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import com.example.whether2.R
 import com.example.whether2.databinding.FragmentMainBinding
 
 class mainFragment : Fragment() {
+    private lateinit var pLauncher: ActivityResultLauncher<String>
     private lateinit var binding: FragmentMainBinding
 
     override fun onCreateView(
@@ -17,6 +22,24 @@ class mainFragment : Fragment() {
     ): View {
         binding = FragmentMainBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        checkPermission()
+    }
+
+   private fun permissionListener(){
+       pLauncher = registerForActivityResult(
+           ActivityResultContracts.RequestPermission()){
+           Toast.makeText(activity, "Permission is $it", Toast.LENGTH_LONG).show()
+       }
+   }
+    private fun checkPermission(){
+        if(!isPermissionGranted(Manifest.permission.ACCESS_FINE_LOCATION)){
+            permissionListener()
+            pLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
+        }
     }
 
     companion object {
