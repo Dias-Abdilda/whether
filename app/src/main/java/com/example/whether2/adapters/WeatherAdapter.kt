5be63473291891bf542576be4fa8d42b491b75dc
A3eke <200103404@stu.sdu.edu.kz>
2022-11-22 17:11:10 +0545

    create class comparator and implement some members to weather adapter class

--- app/src/main/java/com/example/whether2/adapters/WeatherAdapter.kt
+++ app/src/main/java/com/example/whether2/adapters/WeatherAdapter.kt
@@ -1,13 +1,18 @@
 package com.example.whether2.adapters
 
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
+import com.example.whether2.R
 import com.example.whether2.databinding.ListItemBinding
 
-class WeatherAdapter: ListAdapter<WeatherModel, WeatherAdapter.Holder>() {
-    class Holder(view: View): RecyclerView.ViewHolder(view){
+class WeatherAdapter: ListAdapter<WeatherModel, WeatherAdapter.Holder>(Comparator()) {
+    class Holder(view: View) : RecyclerView.ViewHolder(view) {
         val binding = ListItemBinding.bind(view)
+
         fun bind(item: WeatherModel) = with(binding) {
             tvDate.text = item.time
             tvCondition.text = item.condition
@@ -15,4 +20,24 @@ class WeatherAdapter: ListAdapter<WeatherModel, WeatherAdapter.Holder>() {
 
         }
     }
+
+    class Comparator : DiffUtil.ItemCallback<WeatherModel>() {
+        override fun areItemsTheSame(oldItem: WeatherModel, newItem: WeatherModel): Boolean {
+            return oldItem == newItem
+        }
+
+        override fun areContentsTheSame(oldItem: WeatherModel, newItem: WeatherModel): Boolean {
+            return oldItem == newItem
+        }
+    }
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Holder {
+            val view  = LayoutInflater.from(parent.context).inflate(R.layout.list_item, parent , false)
+            return Holder(view)
+    }
+
+    override fun onBindViewHolder(holder: Holder, position: Int) {
+
+    }
 }
+
+
