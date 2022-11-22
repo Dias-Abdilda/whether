6f462ef7f8a093018980f2539275219157ed6b43
Assanali_Rakhmetulla <200107012@stu.sdu.edu.kz>
2022-11-22 11:30:06 +0600

    My commit

--- app/src/main/java/com/example/whether2/MainViewModel.kt
+++ app/src/main/java/com/example/whether2/MainViewModel.kt
@@ -2,8 +2,9 @@ package com.example.whether2
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.whether2.adapters.WeatherModel
 
 class MainViewModel : ViewModel() {
-    val liveDataCurrent = MutableLiveData <String>()
-    val liveDataList = MutableLiveData<String>()
+    val liveDataCurrent = MutableLiveData <WeatherModel>()
+    val liveDataList = MutableLiveData<List<WeatherModel>>()
 }
